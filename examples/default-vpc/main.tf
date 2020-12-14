4676f978f148c94f00d47b3aefcaa908b9f96be3
Marcin Wyszynski <marcinw@spacelift.io>
2020-12-14 15:12:33 +0100

    Test in Spacelift, add PR template (#6)

---
+++ examples/default-vpc/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws    = { source = "hashicorp/aws" }
+    random = { source = "hashicorp/random" }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_security_group" "this" {
+  name = "default"
+}
+
+data "aws_vpc" "this" {
+  default = true
+}
+
+data "aws_subnet_ids" "this" {
+  vpc_id = data.aws_vpc.this.id
+}
+
+resource "random_pet" "this" {}
+
+module "this" {
+  source = "../../"
+
+  configuration   = ""
+  security_groups = [data.aws_security_group.this.id]
+  vpc_subnets     = data.aws_subnet_ids.this.ids
+  worker_pool_id  = random_pet.this.id
+}
