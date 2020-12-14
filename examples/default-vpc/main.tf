0c883ca9ab98934a18bde1257e782184df7d2ceb
Marcin Wyszynski <marcin.pixie@gmail.com>
2020-12-14 14:39:44 +0100

    Fix test

--- examples/default-vpc/main.tf
+++ examples/default-vpc/main.tf
@@ -1,11 +1,29 @@
 terraform {
   required_providers {
-    aws = { source = "hashicorp/aws" }
+    aws    = { source = "hashicorp/aws" }
+    random = { source = "hashicorp/random" }
   }
 }
+
+data "aws_security_group" "this" {
+  name = "default"
+}
+
+data "aws_vpc" "this" {
+  default = true
+}
+
+data "aws_subnet_ids" "this" {
+  vpc_id = data.aws_vpc.this.id
+}
+
+resource "random_pet" "this" {}
 
 module "this" {
   source = "../../"
 
-  worker_pool_id = "test"
+  configuration   = ""
+  security_groups = [data.aws_security_group.this.id]
+  vpc_subnets     = data.aws_subnet_ids.this.ids
+  worker_pool_id  = random_pet.this.id
 }
