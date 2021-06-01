aaafd10de1ce6e81691837825f5b91d504fe7d8b
Marcin Prokop <marcin@prokop.co>
2021-06-01 16:41:56 +0200

    Verify launcher binary checksum

--- asg.tf
+++ asg.tf
@@ -15,6 +15,35 @@ yum update-minimal --security -y 1>>/var/log/spacelift/info.log 2>>/var/log/spac
 echo "Downloading Spacelift launcher" >> /var/log/spacelift/info.log
 curl https://downloads.${var.domain_name}/spacelift-launcher --output /usr/bin/spacelift-launcher 2>>/var/log/spacelift/error.log
 
+echo "Importing public GPG key" >> /var/log/spacelift/info.log
+curl -sS https://keys.openpgp.org/vks/v1/by-fingerprint/175FD97AD2358EFE02832978E302FB5AA29D88F7 | gpg --import 2>>/var/log/spacelift/error.log
+
+echo "Downloading Spacelift launcher checksum file and signature" >> /var/log/spacelift/info.log
+curl https://downloads.${var.domain_name}/spacelift-launcher_SHA256SUMS --output spacelift-launcher_SHA256SUMS 2>>/var/log/spacelift/error.log
+curl https://downloads.${var.domain_name}/spacelift-launcher_SHA256SUMS.sig --output spacelift-launcher_SHA256SUMS.sig 2>>/var/log/spacelift/error.log
+
+echo "Verifying checksum signature..." >> /var/log/spacelift/info.log
+gpg --verify spacelift-launcher_SHA256SUMS.sig 1>>/var/log/spacelift/info.log 2>>/var/log/spacelift/error.log
+
+retStatus=$?
+if [ $retStatus -eq 0 ]; then
+    echo "OK!" >> /var/log/spacelift/info.log
+else
+    return $retStatus
+fi
+
+CHECKSUM=$(cut -f 1 -d ' ' spacelift-launcher_SHA256SUMS)
+rm spacelift-launcher_SHA256SUMS
+RELEASE_SHA=$(shasum -a 256 /usr/bin/spacelift-launcher | cut -f 1 -d ' ')
+
+echo "Verifying launcher binary..." >> /var/log/spacelift/info.log
+if [[ "$CHECKSUM" == "$RELEASE_SHA" ]]; then
+  echo "OK!" >> /var/log/spacelift/info.log
+else
+  echo "Checksum and launcher binary hash did not match" >> /var/log/spacelift/error.log
+  return 1
+fi
+
 echo "Making the Spacelift launcher executable" >> /var/log/spacelift/info.log
 chmod 755 /usr/bin/spacelift-launcher 2>>/var/log/spacelift/error.log
 
