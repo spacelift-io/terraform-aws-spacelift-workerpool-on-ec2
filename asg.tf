bea4c54480a65988bbc8bd145bbdf534702d131e
Marcin Wyszynski <marcin.pixie@gmail.com>
2021-09-16 19:45:24 +0200

    Do not fail on yum update error

--- asg.tf
+++ asg.tf
@@ -10,7 +10,7 @@ set -e
 
   user_data_tail = <<EOF
 echo "Updating packages (security)" >> /var/log/spacelift/info.log
-yum update-minimal --security -y 1>>/var/log/spacelift/info.log 2>>/var/log/spacelift/error.log
+yum update-minimal --security -y 1>>/var/log/spacelift/info.log 2>>/var/log/spacelift/error.log || true
 
 echo "Downloading Spacelift launcher" >> /var/log/spacelift/info.log
 curl https://downloads.${var.domain_name}/spacelift-launcher --output /usr/bin/spacelift-launcher 2>>/var/log/spacelift/error.log
