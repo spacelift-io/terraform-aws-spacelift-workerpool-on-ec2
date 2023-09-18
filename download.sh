739ad731aafe8dc405b04d1b44628cba6ba3fa9f
Emin Alemdar <77338109+eminalemdar@users.noreply.github.com>
2023-09-18 17:52:59 +0300

    Updated the bash script

--- download.sh
+++ download.sh
@@ -7,7 +7,5 @@ local_path=$2
 
 curl -L -o ${local_path}/lambda.zip https://github.com/spacelift-io/ec2-workerpool-autoscaler/releases/download/v${code_version}/ec2-workerpool-autoscaler_${code_version}_linux_amd64.zip
 
-pwd
-mkdir lambda
-cd lambda
-unzip ../../lambda.zip
+cd ..
+unzip lambda.zip
