1cc268a9c864b61c2700688f375ab48183769232
Emin Alemdar <77338109+eminalemdar@users.noreply.github.com>
2023-09-26 18:09:34 +0300

    Removed Local Path variable

--- autoscaler.tf
+++ autoscaler.tf
@@ -12,14 +12,14 @@ resource "aws_ssm_parameter" "spacelift_api_key_secret" {
 resource "null_resource" "download" {
   count = var.enable_autoscaling ? 1 : 0
   provisioner "local-exec" {
-    command = "${path.module}/download.sh ${var.autoscaler_version} ${var.local_path}"
+    command = "${path.module}/download.sh ${var.autoscaler_version}"
   }
 }
 
 data "archive_file" "binary" {
   count       = var.enable_autoscaling ? 1 : 0
   type        = "zip"
-  source_file = "${var.local_path}/bootstrap"
+  source_file = "${path.module}/bootstrap"
   output_path = "ec2-workerpool-autoscaler_${var.autoscaler_version}.zip"
   depends_on  = [null_resource.download]
 }
