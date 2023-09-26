7ae9fe9341bf447a17782cf2e4c6fb5c01038362
Emin Alemdar <77338109+eminalemdar@users.noreply.github.com>
2023-09-26 18:30:56 +0300

    Improvement on the module (#53)
    * Removed Local Path variable
    
    * Updated the bash script
    
    * Updated the binary location for the archive data resource
    
    * Updated the binary location
    
    * Updated the module version

--- download.sh
+++ download.sh
@@ -3,9 +3,9 @@ set -ex
 
 # Download the data.
 code_version=$1
-local_path=$2
 
-curl -L -o ${local_path}/lambda.zip https://github.com/spacelift-io/ec2-workerpool-autoscaler/releases/download/${code_version}/ec2-workerpool-autoscaler_linux_amd64.zip
+curl -L -o lambda.zip https://github.com/spacelift-io/ec2-workerpool-autoscaler/releases/download/${code_version}/ec2-workerpool-autoscaler_linux_amd64.zip
 
-cd ../../../../../tmp
-unzip lambda.zip
+mkdir lambda
+cd lambda
+unzip ../lambda.zip
