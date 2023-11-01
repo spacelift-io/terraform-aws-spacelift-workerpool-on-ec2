6f39bbb5caefcbfd72bb90ceb81ad297a07e0e44
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2023-11-01 18:01:22 +0200

    fix: Lambda download during worker pool recreation, remove temp file
    mkdir error: "cannot create directory ‘lambda’: File exists"
    unzip error: replace LICENSE? [y]es, [n]o, [A]ll, [N]one, [r]ename:  NULL

--- download.sh
+++ download.sh
@@ -6,6 +6,7 @@ code_version=$1
 
 curl -L -o lambda.zip https://github.com/spacelift-io/ec2-workerpool-autoscaler/releases/download/${code_version}/ec2-workerpool-autoscaler_linux_amd64.zip
 
-mkdir lambda
+mkdir -p lambda
 cd lambda
-unzip ../lambda.zip
+unzip -o ../lambda.zip
+rm ../lambda.zip
