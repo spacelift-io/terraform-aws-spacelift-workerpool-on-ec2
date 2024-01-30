8634524a8a988750dd276b041157bfe23e9e6505
Loren Gordon <lorengordon@users.noreply.github.com>
2024-01-30 15:19:18 -0800

    Formats code using terraform fmt -recursive

--- examples/autoscaler-s3-package/s3_package.tf
+++ examples/autoscaler-s3-package/s3_package.tf
@@ -5,11 +5,11 @@
 # to host the binary in your own S3 bucket.
 
 resource "aws_s3_bucket" "autoscaler_binary" {
-    bucket_prefix = "spacelift-autoscaler-example-"
+  bucket_prefix = "spacelift-autoscaler-example-"
 }
 
 data "http" "autoscaler_binary" {
-  url   = "https://github.com/spacelift-io/ec2-workerpool-autoscaler/releases/download/${var.autoscaler_version}/ec2-workerpool-autoscaler_linux_${var.autoscaler_architecture}.zip"
+  url = "https://github.com/spacelift-io/ec2-workerpool-autoscaler/releases/download/${var.autoscaler_version}/ec2-workerpool-autoscaler_linux_${var.autoscaler_architecture}.zip"
 }
 
 resource "aws_s3_object" "autoscaler_binary" {
