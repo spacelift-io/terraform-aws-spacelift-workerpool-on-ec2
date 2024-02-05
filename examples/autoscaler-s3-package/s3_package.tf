47c6f751444acc6445d9f6cd2e72c06c3bdefad4
Loren Gordon <lorengordon@users.noreply.github.com>
2024-02-05 11:14:15 -0800

    Includes version in key as change detection mechanism for lambda

--- examples/autoscaler-s3-package/s3_package.tf
+++ examples/autoscaler-s3-package/s3_package.tf
@@ -1,7 +1,7 @@
 # This is just a toy example config that pulls the autoscaler binary from GitHub
 # and hosts it in S3. This setup allows a simple plan/apply to work directly on
 # the example. In actual usage, it would not be recommended to abuse the http data
-# source and aws_s3_object resource in this manner. Instead, use an exernal process
+# source and aws_s3_object resource in this manner. Instead, use an external process
 # to host the binary in your own S3 bucket.
 
 resource "aws_s3_bucket" "autoscaler_binary" {
@@ -13,7 +13,7 @@ data "http" "autoscaler_binary" {
 }
 
 resource "aws_s3_object" "autoscaler_binary" {
-  key            = "autoscaler_lambda.zip"
+  key            = "releases/download/${var.autoscaler_version}/ec2-workerpool-autoscaler_linux_${var.autoscaler_architecture}.zip"
   bucket         = aws_s3_bucket.autoscaler_binary.id
   content_base64 = data.http.autoscaler_binary.response_body_base64
   content_type   = "application/octet-stream"
