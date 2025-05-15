dbd34ad7a8257c1cc5eb14c45727b53f7ae3da64
Joey Stout <joey@apollorion.com>
2025-05-15 09:18:58 -0400

    feat: BYO ssm and secrets manager (#152)

--- checks.tf
+++ checks.tf
@@ -1,23 +1,47 @@
+locals {
+  byo_ssm_creds_supplied = alltrue([
+    local.byo_ssm,
+    can(var.spacelift_api_credentials.api_key_endpoint),
+    can(var.spacelift_api_credentials.api_key_id),
+  ])
+
+  generated_ssm_creds_supplied = alltrue([
+    !local.byo_ssm,
+    can(var.spacelift_api_credentials.api_key_endpoint),
+    can(var.spacelift_api_credentials.api_key_id),
+    can(var.spacelift_api_credentials.api_key_secret),
+  ])
+}
+
 resource "validation_error" "secure_env_vars_or_configuration" {
   condition = !local.has_secure_env_vars && var.configuration == ""
-  summary   = "Either var.secure_env_vars or var.configuration must be set"
+  summary   = "Either var.secure_env_vars, var.byo_secretsmanager, or var.configuration must be set"
   details   = <<EOT
-You must supply either 'secure_env_vars' or 'configuration' to the module.
+You must supply either 'secure_env_vars', 'var.byo_secretsmanager' or 'configuration' to the module.
 EOT
 }
 
 resource "validation_error" "autoscaler_requires_api_token" {
-  condition = var.autoscaling_configuration != null && var.spacelift_api_credentials == null
+  condition = local.autoscaling_enabled && !(local.generated_ssm_creds_supplied || local.byo_ssm_creds_supplied)
   summary   = "Spacelift API token is required for autoscaler"
   details   = <<EOT
-The autoscaler requires api credentials to be passed in the 'spacelift_api_credentials' variable.
+The autoscaler requires api credentials to be passed in the 'spacelift_api_credentials' variable or var.byo_ssm provided.
 EOT
 }
 
 resource "validation_error" "lifecycle_manager_requires_api_token" {
-  condition = local.lifecycle_manager_enabled && var.spacelift_api_credentials == null
+  condition = local.lifecycle_manager_enabled && !(local.generated_ssm_creds_supplied || local.byo_ssm_creds_supplied)
   summary   = "Spacelift API token is required for instance refresh support"
   details   = <<EOT
-The instance refresh functionality requires api credentials to be passed in the 'spacelift_api_credentials' variable.
+The instance refresh functionality requires api credentials to be passed in the 'spacelift_api_credentials' variable or var.byo_ssm provided.
+EOT
+}
+
+resource "validation_error" "cannot_provide_byo_secretsmanager_and_secure_env_vars" {
+  condition = local.byo_secretsmanager && (var.secure_env_vars != null && length(var.secure_env_vars) > 0)
+  summary   = "Cannot provide both 'byo_secretsmanager' and 'secure_env_vars'"
+  details   = <<EOT
+The 'byo_secretsmanager' and 'secure_env_vars' variables are mutually exclusive.
+Please provide only one of them.
 EOT
 }
---
+++ examples/byo-ssm-secretsmanager-with-autoscaling-and-lifecycle/main.tf
@@ -0,0 +1,108 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "< 6.0"
+    }
+
+    random = { source = "hashicorp/random" }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      TfModule = "terraform-aws-spacelift-workerpool-on-ec2"
+      TestCase = "Autoscaler"
+    }
+  }
+}
+
+data "aws_vpc" "this" {
+  default = true
+}
+
+data "aws_security_group" "this" {
+  name   = "default"
+  vpc_id = data.aws_vpc.this.id
+}
+
+data "aws_subnets" "this" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.this.id]
+  }
+}
+
+// This is just here for testing purposes.
+resource "random_pet" "byo" {
+  prefix = "byo"
+  length = 8
+}
+
+resource "aws_secretsmanager_secret" "byo" {
+  name = random_pet.byo.id
+}
+
+resource "aws_secretsmanager_secret_version" "byo" {
+  secret_id = aws_secretsmanager_secret.byo.id
+  secret_string = jsonencode({
+    SPACELIFT_TOKEN            = "<token-here>"
+    SPACELIFT_POOL_PRIVATE_KEY = "<private-key-here>"
+  })
+}
+
+resource "aws_ssm_parameter" "byo" {
+  name  = random_pet.byo.id
+  type  = "SecureString"
+  value = var.spacelift_api_key_secret
+}
+
+#### Spacelift worker pool ####
+
+module "this" {
+  source = "../../"
+
+  security_groups = [data.aws_security_group.this.id]
+  vpc_subnets     = data.aws_subnets.this.ids
+  worker_pool_id  = var.worker_pool_id
+
+  byo_secretsmanager = {
+    name = aws_secretsmanager_secret.byo.name
+    arn  = aws_secretsmanager_secret.byo.arn
+    keys = [
+      "SPACELIFT_TOKEN",
+      "SPACELIFT_POOL_PRIVATE_KEY"
+    ]
+  }
+
+  byo_ssm = {
+    name = aws_ssm_parameter.byo.name
+    arn  = aws_ssm_parameter.byo.arn
+  }
+
+  autoscaling_configuration = {
+    max_create          = 5
+    max_terminate       = 5
+    architecture        = "arm64" # ~ 20% cheaper than amd64
+    schedule_expression = "rate(1 minute)"
+    timeout             = 60
+  }
+
+  spacelift_api_credentials = {
+    api_key_endpoint = var.spacelift_api_key_endpoint
+    api_key_id       = var.spacelift_api_key_id
+  }
+
+  instance_refresh = {
+    strategy = "Rolling"
+    preferences = {
+      instance_warmup        = 60
+      min_healthy_percentage = 50
+      max_healthy_percentage = 100
+    }
+    triggers = ["tag"]
+  }
+}
